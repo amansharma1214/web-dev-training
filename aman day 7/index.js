e49926628bae5d6a620795d1a9f34593760903a9
amansharma1214 <amankumar970954@gmail.com>
2024-12-03 15:53:25 +0530

    Add files via upload

---
+++ aman day 7/index.js
@@ -0,0 +1,50 @@
+// const heading = document.
+// getElementsByClassName("desc");
+// for(let i=0;i < heading.length; i++){
+//     if( i%2===0){
+//     heading[i].innerText = "even";
+//     }
+//     else{
+//         heading[i].innerText = "odd";
+//     }
+// }
+
+    
+// desc[0].innerText = " welcome";
+// desc[1].innerText = " welcome";
+// desc[2].innerText = " welcome";
+// desc[3].innerText = " welcome";
+// desc[4].innerText = " welcome";
+// desc[5].innerText = " welcome";
+// desc[6].innerText = " welcome";
+// desc[7].innerText = " welcome";
+// desc[8].innerText = " welcome";
+// desc[9].innerText = " welcome";
+// desc[10].innerText = " welcome";
+// console.log(desc);
+//  select:
+// const h1Ele = document.getElementById("heading");
+// // new content veriable:
+// const newcontent = "----12345----"
+// // assign value to tag:
+// h1Ele.innerText = newcontent;
+
+
+
+// // Array
+// // index :    0 ,1 ,2,3,4,5,6,7,8,9
+// const arr = [ 10 ,9 ,8,7,6,5,4,3,2,1];
+// const firstelement = arr[7];
+// console.log(firstelement);
+const arr =[ 132.13,14,121,147,123,159,325,365,154,169,425,122,456,854]
+for(let i=0;i < heading.length; i++){
+        if( i%2===0){
+     heading[i].innerText = "even";
+       }
+        else{
+           heading[i].innerText = "odd";
+       }
+     }
+    
+
+}
