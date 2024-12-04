904eb5b4ab76ef86449dafd79ba1cb1324e01c69
amansharma1214 <amankumar970954@gmail.com>
2024-12-04 15:56:13 +0530

    Add files via upload
    day 8

---
+++ day 8/index.js
@@ -0,0 +1,45 @@
+// // // console.log(---hello everyone---);
+// let arr=[1,5,2,6,9,8,7,4,"hello","hi" ,"welcome"]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+
+// }
+// arr.push("Aman");
+// console.log("--------------");
+// arr.pop();
+//     // arr.splice(start,count,addon)
+//     arr.splice(2,1);
+// const newArr=[5,5,5,3,6,9,8,5,7,1,2,];
+// let count=0
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+//     if(arr[i]%2 === 0)count++;
+//     console.log(count);
+// }
+// let arr=[1,4,5,6,73,2,4];
+
+// let sum=0;
+// for(let i=0;i<arr.length;i++){
+//       console.log(arr[i]);
+//          if(arr[i]%2 === 1){
+//             sum=sum+arr[i];
+//          }
+//          console.log(sum);
+//      }
+let student_id_card={
+    name:"aman",
+    rollnumber:"12655",
+    address:" wgwyy",
+    sec:"A",
+    course:"B-tech"
+
+}
+
+const course =student_id_card.course
+const name =student_id_card.name
+const rollnumber =student_id_card.rollnumber
+const sec =student_id_card.sec
+console.log(sec);
+console.log(student_id_card);
+student_id_card.result="pass"
+console.log(student_id_card.result);
