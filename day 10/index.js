2e0009e7d677421f8338d7d28aecb16d9b347596
amansharma1214 <amankumar970954@gmail.com>
2024-12-06 15:44:09 +0530

    Add files via upload
    day 10

---
+++ day 10/index.js
@@ -0,0 +1,43 @@
+// function sum(a,b){
+//     const sum = a+b;
+//     console.log( sum);
+
+// }
+// const counter = document.
+// getElementById("counter");
+// let count = 0;
+// function incrimentValue(){
+//     count++;
+//     counter.innerText = count;
+// }
+
+// function decrement(){
+//     count--;
+//     counter.innerText = count;
+// }
+// const a =5;
+// const b =6;
+// console.log("---val of a:",a);
+// try{
+//     a=7;
+//     b=3;
+// console.log("---val of a:",a);
+// } catch(error){
+//     console.log(error);
+// }
+// console.log("---val of b:",b);
+
+// function mypromisefun(res,rej){
+//     let delay = 10000;
+//     while("delay");
+//     rej(" Hello World");
+
+// }
+const mypromise = new Promise(solve);
+function solve(fun1,fun2)
+{
+    let delay = 10000;
+        while("delay");
+    rej(" Hello World");}
+    
+console.log(mypromise);
